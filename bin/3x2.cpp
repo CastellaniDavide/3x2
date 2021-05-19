8329a7c9575fae0bbe48854b155646fbde6b6ff6
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-05-19 15:58:22 +0200

    Created bin/3x2.cpp

---
+++ bin/3x2.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file 3x2.cpp
+ *
+ * @version 01.01 2021519
+ *
+ * @brief https://training.olinfo.it/#/task/ois_3x2/statement
+ *
+ * @ingroup 3x2
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
